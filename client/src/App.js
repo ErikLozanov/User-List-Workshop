06e471b37e8e04aa84c8755f7c81835bf1c74886
ErikLozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-08-18 13:38:58 +0300

    Added Search and UserList component.

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import './App.css';
+import UserList from "./components/UserList";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main className='main'>
+      <section className="card users-container">
+        <Search />
+        <UserList />
+    </section>
+
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
